acc1df0564408809f4fcbbd5209553c1e3e218de
Mukesh Kumar <mukeshkmar1905@gmail.com>
2014-02-19 16:02:26 +0530

    Mingle

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+ 
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
+app.use(express.static(__dirname + '/public'));
+var io = require('socket.io').listen(app.listen(port));
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the Mingle' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+console.log("Listening on port " + port);
