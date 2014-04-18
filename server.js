e210d424573e578ede7f1bb17e2cdcbda9f64dc5
Mukesh Kumar <mukeshkmar1905@gmail.com>
2014-04-18 22:57:41 +0530

    Added route for rooms

--- server.js
+++ server.js
@@ -10,14 +10,31 @@ app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 app.get("/", function(req, res){
-    res.render("page");
+	share = generateRoom(6);
+    res.render('page.jade', {shareURL: req.protocol + '://' + req.get('host') + req.path + share, share: share});
+});
+
+app.get('/:room([A-Za-z0-9]{6})', function(req, res) {
+    share = req.params.room;
+    res.render('index.jade', {shareURL: req.protocol + '://' + req.get('host') + '/' + share, share: share});
 });
 app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port,ipaddr));
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the Mingle' });
+    socket.emit('message', { message: 'welcome to Mingle' });
     socket.on('send', function (data) {
         io.sockets.emit('message', data);
     });
 });
 console.log("Listening on port " + port);
+
+function generateRoom(length) {
+    var haystack = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var room = '';
+ 
+    for(var i = 0; i < length; i++) {
+        room += haystack.charAt(Math.floor(Math.random() * 62));
+    }
+ 
+    return room;
+};
