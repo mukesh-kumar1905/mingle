534398ef3fc72bd0d88866c9cece693a6c6f5e23
Mukesh Kumar <mukeshkmar1905@gmail.com>
2014-04-18 22:08:28 +0530

    uncommented grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,15 +29,6 @@ module.exports = function(grunt) {
             },
         },
         watch: {
-            js: {
-                files: [
-                    './bower_components/jquery/src/jquery.js',
-                    './bower_components/bootstrap/dist/js/bootstrap.js',
-                    './bower_components/alertify.js/lib/alertify.js',
-                    './assets/javascript/frontend.js'
-                ],
-                tasks: ['concat:js']
-            },
             less: {
                 files: ['./assets/stylesheets/*.less'],
                 tasks: ['less']
