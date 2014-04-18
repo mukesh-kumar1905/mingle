32bc11c2ee152f23bd3b8e0a97d594e1c85ef69d
Mukesh Kumar <mukeshkmar1905@gmail.com>
2014-04-18 23:16:16 +0530

    added joining rooms

--- server.js
+++ server.js
@@ -26,6 +26,43 @@ io.sockets.on('connection', function (socket) {
 
         io.sockets.emit('message', data);
     });
+    socket.on('join', function(data) {
+        if(data.room in games) {
+            if(typeof games[data.room].player2 != "undefined") {
+                socket.emit('leave');
+                return;
+            }
+            socket.join(data.room);
+            socket.set('room', data.room);
+            socket.set('color', '#FB6B5B');
+            socket.set('pid', -1);
+            games[data.room].player2 = socket
+            // Set opponents
+            socket.set('opponent', games[data.room].player1);
+            games[data.room].player1.set('opponent', games[data.room].player2);
+ 
+            // Set turn
+            socket.set('turn', false);
+            socket.get('opponent', function(err, opponent) {
+                opponent.set('turn', true);
+            });
+ 
+            socket.emit('assign', {pid: 2});
+ 
+        }
+        else {
+            socket.join(data.room);
+            socket.set('room', data.room);
+            socket.set('color', '#FFC333');
+            socket.set('pid', 1);
+            socket.set('turn', false);
+            games[data.room] = {
+                player1: socket,
+                board: [[0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0]],
+            };
+            socket.emit('assign', {pid: 1});
+        }
+    });
 });
 console.log("Listening on port " + port);
 
